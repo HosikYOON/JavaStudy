2d9cfc05f42a951270c9995635ad283606a26c2e
HosikYOON <00ghtlr00@naver.com>
2021-03-19 16:46:31 +0900

    정보 은닉(private)

---
+++ FastC/src/hiding/MyDate.java
@@ -0,0 +1,27 @@
+package hiding;
+
+public class MyDate {
+
+	private int day;
+	private int month;
+	private int year;
+	
+	public void setDay(int day) {
+		if((day >= 1)&& (day<=31))
+			this.day = day;
+		else
+			System.out.println("\'日\'을 잘 못 입력 하셨습니다.");
+	}
+	public void setMonth(int month) {
+		if((month >= 1)&&(month <= 12))
+			this.month = month;
+		else
+			System.out.println("\'月\'을 잘 못 입력 하셨습니다.");
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public void showDate() {
+		System.out.println(this.year +"년\n"+this.month+"월\n"+ this.day+"일\n" );
+	}
+}
---
+++ FastC/src/hiding/MydateTest.java
@@ -0,0 +1,15 @@
+package hiding;
+
+public class MydateTest {
+
+	public static void main(String[] args) {
+
+		MyDate mydate = new MyDate();
+		
+		mydate.setDay(19);
+		mydate.setMonth(03);
+		mydate.setYear(2021);
+		mydate.showDate();
+	}
+
+}
